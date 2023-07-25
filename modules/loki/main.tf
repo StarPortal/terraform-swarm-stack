b584cb43dcee463eb40f71c01955215b974462a2
Aotokitsuruya <contact@aotoki.me>
2023-07-25 23:39:35 +0800

    Add expose port for VIP mode

--- modules/loki/main.tf
+++ modules/loki/main.tf
@@ -77,6 +77,20 @@ resource "docker_service" "this" {
     }
   }
 
+  endpoint_spec {
+    ports {
+      target_port = 3100
+    }
+
+    ports {
+      target_port = 7946
+    }
+
+    ports {
+      target_port = 9095
+    }
+  }
+
   mode {
     replicated {
       replicas = var.replicas
